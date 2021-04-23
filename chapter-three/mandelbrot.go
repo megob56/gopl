fdaa0259e5fa67dddda888338b36861a96d714e0
megob56 <obrien.e.meaghan@gmail.com>
2021-04-23 17:09:21 +0100

    Changed the color of the madelbrot image

--- chapter-three/mandelbrot.go
+++ chapter-three/mandelbrot.go
@@ -36,7 +36,7 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			return color.RGBA{100, 150, 100, 255}
 		}
 	}
 	return color.Black
